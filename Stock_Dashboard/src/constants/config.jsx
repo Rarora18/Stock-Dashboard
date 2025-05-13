7a3f0edd2879014434c84eff08f70887287fb79a
Raghav <ra346238@gmail.com>
2025-05-12 21:41:31 -0700

    Added mock data and chart specific config data

---
+++ Stock_Dashboard/src/constants/config.jsx
@@ -0,0 +1,12 @@
+export const ChartConfig = {
+
+    "1D" : {days: 1, weeks: 0, months: 0, years: 0, resolution: "1"},
+    "1W" : {days: 0, weeks: 1, months: 0, years: 0, resolution: "15"},
+    "1M" : {days: 0, weeks: 0, months: 1, years: 0, resolution: "60"},
+    // "3M" : {days: 0, weeks: 0, months: 3, years: 0 },
+    // "6M" : {days: 0, weeks: 0, months: 6, years: 0 },
+    // "YTD" : {days: 0, weeks: 0, months: 0, years: 0 },
+    "1Y" : {days: 0, weeks: 0, months: 0, years: 1, resolution: "D"},
+//     "5Y" : {days: 0, weeks: 0, months: 0, years: 5 },
+//     "MAX" : {},
+}
