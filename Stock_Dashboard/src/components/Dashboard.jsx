344e7ddcab3c632c2c7865060d8caa692cc6627b
Raghav <ra346238@gmail.com>
2025-05-19 15:44:16 -0700

    Created a new function for searchbar

--- Stock_Dashboard/src/components/Dashboard.jsx
+++ Stock_Dashboard/src/components/Dashboard.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
-const Dashboard = () => {
+
+
+const Searchbar = () => {
     const [search, setSearch] = useState('')
-  return (
-    <div>
-      <div className='searchBar'>
+    return <div>
+      <div className='flex flex-start'>
         <input className='mx-4 h-10 w-80 px-2 shadow-lg rounded-2xl ' type="text" placeholder='Search Stocks' />
         <button className='bg-yellow-600 shadow-lg '> Search</button>
       </div>
+    </div>
+}
+
+
+
+const Dashboard = () => {
+  
+  return (
+    <div>
+    <Searchbar />
 
     </div>
   )
