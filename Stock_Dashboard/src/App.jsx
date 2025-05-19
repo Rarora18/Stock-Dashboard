65ef8bf227ba31d6db6934aba053a11eb749a2d2
Raghav <ra346238@gmail.com>
2025-05-19 14:12:29 -0700

    added Seachbar

--- Stock_Dashboard/src/App.jsx
+++ Stock_Dashboard/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <div>
       <h1>Stock Dashboard</h1>
+      <Dashboard />
     </div>
   )
-  
 }
 
 export default App
---
+++ Stock_Dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className='searchBar'>
+        <input type="text" placeholder='Search Stocks' />
+        <button> Search</button>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
