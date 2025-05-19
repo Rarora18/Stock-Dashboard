5b8f12c6dfbbc9dad80d6fffa43181e1dcf5818c
Raghav <ra346238@gmail.com>
2025-05-19 15:00:17 -0700

    styled searchbar

--- Stock_Dashboard/src/components/Dashboard.jsx
+++ Stock_Dashboard/src/components/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Dashboard = () => {
+    const [search, setSearch] = useState('')
   return (
     <div>
       <div className='searchBar'>
-        <input type="text" placeholder='Search Stocks' />
-        <button> Search</button>
+        <input className='mx-4 h-10 w-80 px-2 shadow-lg rounded-2xl ' type="text" placeholder='Search Stocks' />
+        <button className='bg-yellow-600 shadow-lg '> Search</button>
       </div>
+
     </div>
   )
 }
