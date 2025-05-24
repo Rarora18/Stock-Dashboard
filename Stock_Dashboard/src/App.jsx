f92b76b7cacf88e9dab4d93fc2e2bf172eab4668
Raghav <ra346238@gmail.com>
2025-05-23 22:12:59 -0700

    initialized price and chart functions

--- Stock_Dashboard/src/App.jsx
+++ Stock_Dashboard/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Dashboard from './components/Dashboard'
-
+import Chart from './components/chart'
 function App() {
   return (
     <div>
       <h1 className=''>Stock Dashboard</h1>
       <Dashboard />
+      <Chart />
+
     </div>
   )
 }
---
+++ Stock_Dashboard/src/components/chart.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+const priceT = () => {
+
+    return (
+        <div>
+
+            
+        </div>
+    )
+}
+const chart = () => {
+  return (
+    <div>chart</div>
+  )
+}
+
+
+export default chart
